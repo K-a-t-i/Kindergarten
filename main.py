330883f62b0afc5d04050b56396f1bd62cc0febe
Kathrin Rupp <kathrin.rupp@code.berlin>
2021-05-04 14:49:08 +0200

    created ignore, securtity and documentation.gcloudignore files

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+# Copy of Adams file: 
+# https://github.com/DrAdamRoe/foundations-project-template
+from great_project.website import app  # noqa: F401
+
 # from hello.application import app
 # from froshims.application import app
 # from login.application import app
 # from shows.application import app
-from store.application import app
+#from store.application import app
 
